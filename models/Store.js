70b6a84b466c46161afbbe00b92dce46b1224a4f
chadelofson <chad.elofson@gmail.com>
2021-02-24 21:17:44 -0800

    App

---
+++ models/Store.js
@@ -0,0 +1,130 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+const { stringify } = require('uuid');
+
+const storeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: 'Please enter a store name!',
+    },
+    slug: String,
+    description: {
+      type: String,
+      trim: true,
+    },
+    tags: [String],
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    location: {
+      type: {
+        type: String,
+        default: 'Point',
+      },
+      coordinates: [
+        {
+          type: Number,
+          required: 'You must supply coordinates',
+        },
+      ],
+      address: {
+        type: String,
+        required: 'You must supply an address!',
+      },
+    },
+    photo: String,
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: 'You must supply an author',
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Define our indexes
+storeSchema.index({
+  name: 'text',
+  description: 'text',
+});
+
+storeSchema.index({ location: '2dsphere' });
+
+storeSchema.pre('save', async function (next) {
+  if (!this.isModified('name')) {
+    next(); // skip its
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  // find other store that have a slug
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-o]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
+  next();
+  // TODO make more resiliant so slugs are unique
+});
+
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+  ]);
+};
+
+storeSchema.statics.getTopStores = function () {
+  return this.aggregate([
+    // Lookup Stores and populate their reviews
+    {
+      $lookup: {
+        from: 'reviews',
+        localField: '_id',
+        foreignField: 'store',
+        as: 'reviews',
+      },
+    },
+    // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Add the average reviews field
+    {
+      $project: {
+        photo: '$$ROOT.photo',
+        name: '$$ROOT.name',
+        reviews: '$$ROOT.reviews',
+        slug: '$$ROOT.slug',
+        averageRating: { $avg: '$reviews.rating' },
+      },
+    },
+    // sort it by our new field, highest review first
+    { $sort: { averageRating: -1 } },
+    // limit to at most 10
+    { $limit: 10 },
+  ]);
+};
+
+// Find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // What model to link
+  localField: '_id', // which field on the store
+  foreignField: 'store', // which field on teh review?
+});
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
+
+module.exports = mongoose.model('Store', storeSchema);
